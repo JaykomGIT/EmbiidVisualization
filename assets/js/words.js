f1c78dfe0b7c097f7141c981346015b3d18f2361
Jaykom <72894558+Jaykom@users.noreply.github.com>
2022-05-02 20:17:59 -0400

    added word cloud and fixed some other things

---
+++ assets/js/words.js
@@ -0,0 +1,65 @@
+(function(){
+
+d3.csv("embiidWords.csv", function(data) {
+
+    //var adj = data.adjectives;
+    //console.log(adj);
+// set the dimensions and margins of the graph
+var margin = {top: 10, right: 10, bottom: 10, left: 10},
+    width = 800 - margin.left - margin.right,
+    height = 800 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg = d3.select("#wordCloud").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+// Constructs a new cloud layout instance. It run an algorithm to find the position of words that suits your requirements
+var layout = d3.layout.cloud()
+  .size([width, height])
+  .words(data.map(function(d) { 
+        console.log(d.adjectives);
+      return {text: d.adjectives, size: d.size}; 
+    }))
+    .padding(5)        //space between words
+    .rotate(function() { return ~~(Math.random() * 2) * 90; })
+    .fontSize(function(d) { return d.size * 40; })      // font size of words
+    .on("end", draw);
+
+    layout.start();
+
+
+
+    $("#randomizer").on("click", function (event) {
+        location.reload();
+    })
+ 
+
+// This function takes the output of 'layout' above and draw the words
+// Better not to touch it. To change parameters, play with the 'layout' variable above
+function draw(words) {
+    var ordinal = d3.scaleOrdinal()
+        .domain(data)
+        .range(["#370617","#6a040f","#9d0208","#d00000","#dc2f02","#e85d04","#f48c06","#faa307", "#ffba08"]);
+
+    svg
+      .append("g")
+        .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
+        .selectAll("text")
+          .data(words)
+        .enter().append("text")
+          .style("font-size", function(d) { return d.size; })
+          .attr("fill", function(d){return ordinal(Math.random() * d.size) })
+          .attr("text-anchor", "middle")
+          .style("font-family", "Impact")
+          .attr("transform", function(d) {
+            return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+          })
+          .text(function(d) { return d.text; });
+  }
+
+})
+})();
